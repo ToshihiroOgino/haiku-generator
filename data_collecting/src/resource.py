7c1b4de539630d3016cb6e84bdfa9393cdebc02f
ToshihiroOgino <tonigo0725@gmail.com>
2024-10-31 14:16:31 +0900

    feat save_to_file to create sub directory

--- data_collecting/src/resource.py
+++ data_collecting/src/resource.py
@@ -5,7 +5,8 @@ _DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "resource")
 
 def save_to_file(file_name, data):
     path = os.path.join(_DATA_DIR, file_name)
-    os.makedirs(_DATA_DIR, exist_ok=True)
+    dir = os.path.abspath(os.path.split(path)[0])
+    os.makedirs(dir, exist_ok=True)
     with open(path, "+w") as file:
         file.write(data)
 
