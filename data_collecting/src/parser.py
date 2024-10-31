92b1a5e94add11aba9df5f8b999d524e33966bac
ToshihiroOgino <tonigo0725@gmail.com>
2024-10-31 15:15:53 +0900

    feat: enhance parser to extract haiku list and save to JSON file

--- data_collecting/src/parser.py
+++ data_collecting/src/parser.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from resource import load_from_file
+from resource import load_from_file, save_to_file
 import re
 
 _SEASONS = [
@@ -11,7 +11,7 @@ _SEASONS = [
 ]
 
 
-def extract_kigo_list(html_str: str):
+def __extract_kigo_list(html_str: str):
     soup = BeautifulSoup(html_str, "html.parser")
     kigo_dict = dict()
     REGEXP = re.compile(r"kigo_work_list\.php\?kigo_cd=(\d+)$")
@@ -23,11 +23,54 @@ def extract_kigo_list(html_str: str):
     return kigo_dict
 
 
-if __name__ == "__main__":
+def extract_kigo(verbose=False):
+    data = dict()
     for season in _SEASONS:
         file_name = f"季語_{season}.html"
-        print(f"Loading {file_name}...")
+        if verbose:
+            print(f"Loading {file_name}...")
         html_str = load_from_file(file_name)
+        if verbose:
+            print("Extracting kigo list...")
+        kigo_dict = __extract_kigo_list(html_str)
+        if verbose:
+            print(f"Found {len(kigo_dict)} kigo.")
+        data.update({season: kigo_dict})
+    return data
+
+
+async def __extract_haiku_list(season, kigo_cd):
+    file_name = f"{season}/kigo_{kigo_cd}.html"
+    html_str = load_from_file(file_name)
+    soup = BeautifulSoup(html_str, "html.parser")
+    haiku_list = list()
+    for haiku in soup.find_all(
+        "a", href=lambda href: href and "work_detail.php?cd=" in href
+    ):
+        haiku_list.append(haiku.text)
+    return haiku_list
+
+
+async def extract_haiku(verbose=False):
+    if verbose:
         print("Extracting kigo list...")
-        kigo_dict = extract_kigo_list(html_str)
-        print(f"Found {len(kigo_dict)} kigo.")
+    data = extract_kigo()
+    if verbose:
+        print("Extracting haiku list...")
+    fut = dict()
+    for season, kigo_dict in data.items():
+        for _, cd in kigo_dict.items():
+            fut.update({cd: __extract_haiku_list(season=season, kigo_cd=cd)})
+    for season, kigo_dict in data.items():
+        for kigo, cd in kigo_dict.items():
+            data[season][kigo] = await fut[cd]
+    return data
+
+
+if __name__ == "__main__":
+    import asyncio
+    import json
+
+    data = asyncio.run(extract_haiku(verbose=True))
+    data_str = json.dumps(data, indent=2, ensure_ascii=False)
+    save_to_file("haiku.json", data_str)
