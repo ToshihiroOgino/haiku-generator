38b2bdea1e93ec69a25494daa307227aaa2f941c
ToshihiroOgino <tonigo0725@gmail.com>
2024-10-31 15:15:42 +0900

    feat: add functionality to download haiku pages from kigo data

--- data_collecting/src/downloader.py
+++ data_collecting/src/downloader.py
@@ -1,5 +1,6 @@
 import requests
 from resource import save_to_file
+from parser import extract_kigo
 
 
 def __get_html(url: str) -> str:
@@ -24,4 +25,28 @@ def __download_kigo_list():
         save_to_file(f"季語_{season}.html", html)
 
 
-__download_kigo_list()
+def __download_haiku_from_kigo(kigo_cd, season):
+    url = f"https://haiku-data.jp/kigo_work_list.php?kigo_cd={kigo_cd}"
+    html = __get_html(url)
+    save_to_file(f"{season}/kigo_{kigo_cd}.html", html)
+
+
+def __download_haiku_page(verbose=False):
+    kigo_data = extract_kigo()
+    total_count = sum([len(kigo_dict) for _, kigo_dict in kigo_data.items()])
+    if verbose:
+        print(f"Downloading {total_count} kigo pages...")
+    count = 0
+    for season, kigo_dict in kigo_data.items():
+        for kigo, kigo_cd in kigo_dict.items():
+            if verbose:
+                print(f"Downloading {kigo}({count}/{total_count})...")
+            __download_haiku_from_kigo(kigo_cd, season)
+            count += 1
+
+
+if __name__ == "__main__":
+    if input("Do you want to download the data? (y/n): ") != "y":
+        exit()
+    __download_kigo_list()
+    __download_haiku_page()
