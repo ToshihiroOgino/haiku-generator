5e05eee7715cd4eb68310a24e5b8c8d01eaf193e
ToshihiroOgino <tonigo0725@gmail.com>
2024-11-18 17:01:44 +0900

    feat: update LoadHaikuData to use stats.HaikuData and add SaveVocabularySet function

--- markov_generator/fileio/jsonio.go
+++ markov_generator/fileio/jsonio.go
@@ -2,15 +2,13 @@ package fileio
 
 import (
 	"encoding/json"
-	"markov_generator/domain"
+	"markov_generator/stats"
 	"os"
 
 	"github.com/gookit/slog"
 )
 
-type HaikuData map[domain.Season](map[domain.Kigo]([]domain.Uta))
-
-func LoadHaikuData(path string) (HaikuData, error) {
+func LoadHaikuData(path string) (stats.HaikuData, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
@@ -18,11 +16,34 @@ func LoadHaikuData(path string) (HaikuData, error) {
 	defer file.Close()
 	slog.Info("reading haiku data from " + path)
 	decoder := json.NewDecoder(file)
-	var res HaikuData
+	var res stats.HaikuData
 	err = decoder.Decode(&res)
 	if err != nil {
 		return nil, err
 	}
 	slog.Info("haiku data loaded")
 	return res, nil
+}
+
+func SaveVocabularySet(path string, vocab stats.VocabularySet) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	slog.Info("saving vocabulary set to " + path)
+
+	jsonEncodable := map[string]stats.VocabID{}
+	for k, v := range vocab {
+		jsonEncodable[k.String()] = v.ID
+	}
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "\t")
+	err = encoder.Encode(jsonEncodable)
+	if err != nil {
+		return err
+	}
+	slog.Info("vocabulary set saved")
+	return nil
 }
