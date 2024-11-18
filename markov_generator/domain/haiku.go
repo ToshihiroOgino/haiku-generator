bfbe61429f1e8c04cd9a16afc5e39e1f55e1f003
ToshihiroOgino <tonigo0725@gmail.com>
2024-11-18 17:01:07 +0900

    feat: add Uta type methods for validation and cleaning of haiku strings

--- markov_generator/domain/haiku.go
+++ markov_generator/domain/haiku.go
@@ -1,5 +1,24 @@
 package domain
 
+import "regexp"
+
 type Uta string
 type Kigo string
 type Season string
+
+func (u Uta) String() string {
+	return string(u)
+}
+
+var invalidSymbols = regexp.MustCompile(`[「『＼≪\?？！…“♯♭、〇〈0-9０-９a-zA-Zａ-ｚＡ-Ｚ～ﾐ＝・]`)
+
+func (u Uta) IsInvalid() bool {
+	return invalidSymbols.MatchString(u.String())
+}
+
+var bracket = regexp.MustCompile(`[(（][^)）]+[)）]`)
+var removableSymbols = regexp.MustCompile(`[／　/\*]`)
+
+func (u Uta) GetCleaned() Uta {
+	return Uta(bracket.ReplaceAllString(removableSymbols.ReplaceAllString(u.String(), ""), ""))
+}
